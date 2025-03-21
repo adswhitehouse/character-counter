dd7fc92565d1d9315feff4be3c7f48a7f145dc2a
Adam Whitehouse <adswhitehouse95@gamil.com>
2025-03-21 17:03:52 +0000

    added exclude spaces functionality

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let characterCount = document.querySelector(".jsCharacterCount");
 let wordCount = document.querySelector(".jsWordCount");
 let sentenceCount = document.querySelector(".jsSentenceCount");
 let textarea = document.querySelector("textarea");
+let excludeSpaces = document.querySelector(".jsExcludeSpaces");
+let totalCharacters = document.querySelector(".jsTotalCharacters");
+
+// Displays relative count numbers
+function displayCounts(unit, count) {
+  count.textContent = unit < 10 ? `0${unit}` : unit;
+}
 
 // On textarea keyup...
 textarea.addEventListener("keyup", () => {
@@ -20,6 +27,23 @@ textarea.addEventListener("keyup", () => {
   displayCounts(sentences, sentenceCount);
 });
 
-function displayCounts(unit, count) {
-  count.textContent = unit < 10 ? `0${unit}` : unit;
-}
+// Is exclude spaces toggled? True/false
+let excludeSpacesActive = false;
+// On exclude spaces button click
+excludeSpaces.addEventListener("click", () => {
+  // Define textarea value and textarea value without spaces
+  let text = textarea.value;
+  let textNoSpaces = textarea.value.replace(/\s+/g, "");
+  // If not active - toggle exclude spaces on and update display... else revert to normal
+  if (!excludeSpacesActive) {
+    excludeSpacesActive = true;
+    excludeSpaces.classList.add("setting-btn-active");
+    totalCharacters.textContent += " (no-space)";
+    characterCount.textContent = textNoSpaces.length;
+  } else {
+    excludeSpacesActive = false;
+    excludeSpaces.classList.remove("setting-btn-active");
+    totalCharacters.textContent = "Total Characters";
+    displayCounts(text.length, characterCount);
+  }
+});
