741e0cba1a766eded655726f93c3b2781a1ed248
Adam Whitehouse <adswhitehouse95@gamil.com>
2025-03-21 17:44:41 +0000

    added functionality for reading time display

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let sentenceCount = document.querySelector(".jsSentenceCount");
 let textarea = document.querySelector("textarea");
 let excludeSpaces = document.querySelector(".jsExcludeSpaces");
 let totalCharacters = document.querySelector(".jsTotalCharacters");
+let readingTime = document.querySelector(".jsReadingTime");
 
 // Displays relative count numbers
 function displayCounts(unit, count) {
@@ -14,32 +15,47 @@ function displayCounts(unit, count) {
 // On textarea keyup...
 textarea.addEventListener("keyup", () => {
   let text = textarea.value;
+
   // Display Character count
   displayCounts(text.length, characterCount);
+
   // Display word count
   let words = text.trim().split(/\s+/).length;
   displayCounts(words, wordCount);
+
   // Display Sentence count
   let sentences = text
     .trim()
     .split(/[.!?]+/)
     .filter(Boolean).length;
   displayCounts(sentences, sentenceCount);
+
+  // Display reading time
+  let minutesReading = Math.floor(text.length / 1000);
+  if (text.length === 0) {
+    readingTime.textContent = "0 minutes";
+  } else if (minutesReading < 1) {
+    readingTime.textContent = "< 1 minute";
+  } else if (minutesReading < 2) {
+    readingTime.textContent = "1 minute";
+  } else {
+    readingTime.textContent = `${minutesReading} minutes`;
+  }
 });
 
-// Is exclude spaces toggled? True/false
 let excludeSpacesActive = false;
 // On exclude spaces button click
 excludeSpaces.addEventListener("click", () => {
   // Define textarea value and textarea value without spaces
   let text = textarea.value;
   let textNoSpaces = textarea.value.replace(/\s+/g, "");
+
   // If not active - toggle exclude spaces on and update display... else revert to normal
   if (!excludeSpacesActive) {
     excludeSpacesActive = true;
     excludeSpaces.classList.add("setting-btn-active");
     totalCharacters.textContent += " (no-space)";
-    characterCount.textContent = textNoSpaces.length;
+    displayCounts(textNoSpaces.length, characterCount);
   } else {
     excludeSpacesActive = false;
     excludeSpaces.classList.remove("setting-btn-active");
